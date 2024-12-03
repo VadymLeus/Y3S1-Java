c8238a49c579b462645d4ea6d78e1f41a119c244
VadymLeus <146115373+VadymLeus@users.noreply.github.com>
2024-12-03 13:59:40 +0200

    Delete Java-LAB-11 directory

--- Java-LAB-11/src/TASK_04/Main.java
+++
@@ -1,8 +0,0 @@
-package TASK_04;
-public class Main {
-    public static void main(String[] args) {
-        ProductManager.addProducts();
-        ProductManager.displayProductsByCategory("Clothing");
-        ProductManager.deleteAllProducts();
-    }
-}
--- Java-LAB-11/src/TASK_05/Main.java
+++
@@ -1,6 +0,0 @@
-package TASK_05;
-public class Main {
-    public static void main(String[] args) {
-        TransactionHandler.handleTransactions();
-    }
-}
