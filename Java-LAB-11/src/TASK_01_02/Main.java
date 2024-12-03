fec48ddbac913ec150fc72c7ec8b07259423f3ed
VadymLeus <146115373+VadymLeus@users.noreply.github.com>
2024-12-03 13:52:52 +0200

    Java-LAB-11

---
+++ Java-LAB-11/src/TASK_01_02/Main.java
@@ -0,0 +1,7 @@
+package TASK_01_02;
+public class Main {
+    public static void main(String[] args) {
+        DatabaseConnector dbConnector = new DatabaseConnector();
+        dbConnector.connect();
+    }
+}
