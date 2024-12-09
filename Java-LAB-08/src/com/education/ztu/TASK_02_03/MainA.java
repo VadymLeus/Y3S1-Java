f2dabd931ff38bd0fe688bcfc50757e499d52c39
VadymLeus <146115373+VadymLeus@users.noreply.github.com>
2024-12-10 01:33:43 +0200

    Java-LAB-08

---
+++ Java-LAB-08/src/com/education/ztu/TASK_02_03/MainA.java
@@ -0,0 +1,25 @@
+package TASK_02_03;
+import java.util.function.Predicate;
+
+public class MainA {
+    public static void main(String[] args) {
+        Predicate<String> isNumeric = str -> {
+            try {
+                Double.parseDouble(str);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        };
+
+        System.out.println(isNumeric.test("123")); // true
+        System.out.println(isNumeric.test("abc")); // false
+
+        // Використання and()
+        Predicate<String> isNotEmpty = str -> !str.isEmpty();
+        Predicate<String> isValidNumber = isNumeric.and(isNotEmpty);
+
+        System.out.println(isValidNumber.test("123")); // true
+        System.out.println(isValidNumber.test(""));    // false
+    }
+}
